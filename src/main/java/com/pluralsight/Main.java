2a8c00044b6469b55c5d02acc9ca71b961d920d9
jihaddrake <ringod40@gmail.com>
2026-04-24 09:33:57 -0400

    Added class for products and switch case for the menu

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,46 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+   public static void main(String[] args) {
+       Scanner uKey = new Scanner(System.in);
+
+       System.out.println("Welcome to the online store");
+       System.out.println("Select from the following option");
+       System.out.println("\t1 Display products");
+       System.out.println("\t2 Display cart");
+       System.out.println("\t0 Exit");
+       System.out.print("Enter your selection");
+
+       int userOption = uKey.nextInt();
+
+       switch(userOption) {
+           case 1:
+               displayProdocuts();
+           case 2:
+               displayCart();
+           case 0:
+               System.out.println("Thanks for visiting");
+               System.exit(0);
+               break;
+       }
+
+
+
+
+
+      //Customers should be able to view all products
+       //Arraylist, File reader, method .getProducts
+
+       // They should also be able to search by Product Name, Price or Department
+       // Method to search up products through name price or deparment so a switch case may have to be made
+       //while loop probably
+
+
+      // Customers should be able to add products to their cart
+       // Hash map with the .put or add
+
+      // Customers should be able to remove products from their cart
+    }  //. remove with hashmap
 }
--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
 public class Product {
+    private String productId;
+    private String productName;
+    private double productPrice;
+    private String productDepo;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public String getProductDepo() {
+        return productDepo;
+    }
 }
