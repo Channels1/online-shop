b54e5912fcca0f434884c1550a5264b10c3c4a2b
jihaddrake <ringod40@gmail.com>
2026-04-24 09:44:55 -0400

    Added in while loop

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,44 +3,39 @@ package com.pluralsight;
 import java.util.Scanner;
 
 public class Main {
-   public static void main(String[] args) {
-       Scanner uKey = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner uKey = new Scanner(System.in);
+        while (true) {
 
-       System.out.println("Welcome to the online store");
-       System.out.println("Select from the following option");
-       System.out.println("\t1 Display products");
-       System.out.println("\t2 Display cart");
-       System.out.println("\t0 Exit");
-       System.out.print("Enter your selection");
+            System.out.println("Welcome to the online store");
+            System.out.println("Select from the following option");
+            System.out.println("\t1 Display products");
+            System.out.println("\t2 Display cart");
+            System.out.println("\t0 Exit");
+            System.out.print("Enter your selection");
 
-       int userOption = uKey.nextInt();
+            int userOption = uKey.nextInt();
+            uKey.nextLine();
 
-       switch(userOption) {
-           case 1:
-               displayProdocuts();
-           case 2:
-               displayCart();
-           case 0:
-               System.out.println("Thanks for visiting");
-               System.exit(0);
-               break;
-       }
-
-
-
-
-
-      //Customers should be able to view all products
-       //Arraylist, File reader, method .getProducts
-
-       // They should also be able to search by Product Name, Price or Department
-       // Method to search up products through name price or deparment so a switch case may have to be made
-       //while loop probably
+            switch (userOption) {
+                case 1:
+                    // displayProdocuts();
+                    break;
+                case 2:
+                    // displayCart();
+                    break;
+                case 0:
+                    System.out.println("Thanks for visiting");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Error!");
+                    uKey.nextLine();
+            }
 
 
-      // Customers should be able to add products to their cart
-       // Hash map with the .put or add
 
-      // Customers should be able to remove products from their cart
-    }  //. remove with hashmap
+
+        }
+    }
 }
